930d281445f688196fd612825d72c2040f29b740
greedyhao <hao_kr@163.com>
2020-12-12 16:22:47 +0800

    hal_sdio 可以完成 sd 卡初始化

--- libraries/hal_libraries/ab32vg1_hal/include/ab32vg1_ll_sdio.h
+++ libraries/hal_libraries/ab32vg1_hal/include/ab32vg1_ll_sdio.h
@@ -11,8 +11,14 @@
 
 struct sdio_init
 {
-    uint32_t tmp;
+    uint32_t clock_power_save;      /*!< Specifies whether SDMMC Clock output is enabled or
+                                        disabled when the bus is idle.                                      */
+    uint32_t clock_div;             /*!< Specifies the clock frequency of the SDMMC controller.
+                                      This parameter can be a value between Min_Data = 0 and Max_Data = 255 */
 };
 typedef struct sdio_init* sdio_init_t;
+
+#define SDMMC_CLOCK_POWER_SAVE_DISABLE      (0x00u)
+#define SDMMC_CLOCK_POWER_SAVE_ENABLE       (0x01u)
 
 #endif
